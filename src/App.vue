25ff777d5abded4c2567ece146f7bf49b1b1f31f
Andreu Micó <andreuolleria@gmail.com>
2025-06-02 22:10:42 +0200

    router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
  <Navbar />
- <Hero title="Test title2" />
- <HomeCards />
- <JobListings :limit="3" :show-button="true" />
+ <RouterView />
 </template>
 
 <style scoped>
