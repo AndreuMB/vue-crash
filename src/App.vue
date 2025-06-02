12ff8461fc17c13616dc8ad349abdf3136f7df43
Andreu Micó <andreuolleria@gmail.com>
2025-06-02 20:42:25 +0200

    browse jobs view

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
 import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
  <Navbar />
  <Hero title="Test title2" />
  <HomeCards />
+ <JobListings :limit="3" :show-button="true" />
 </template>
 
 <style scoped>
